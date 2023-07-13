d452427a45a4f90f493b1f4b887ba48622edb0a1
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-13 17:07:34 +0200

    feat: error page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom"
+import Header from './Header'
+import Footer from './Footer'
+import '../assets/styles/Error.scss'
 
 function Error() {
     return (
+        <>
+        <Header/>
         <div className="container">
-            <p>404</p>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <p><Link to="/">Retourner sur la page d'accueil</Link></p>
+            <p className="container__error">404</p>
+            <p className="container__message">Oups! La page que vous demandez n'existe pas.</p>
+            <p className="container__link"><Link to="/">Retourner sur la page d'accueil</Link></p>
         </div>
+        <Footer/>
+        </>
     )
 }
 
