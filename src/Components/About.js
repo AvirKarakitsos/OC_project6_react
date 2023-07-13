689a8607206ab69b00f86b53cd0cd502cd98e04d
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-13 21:07:49 +0200

    feat: error module scss

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,22 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import image from "../assets/images/image-source-2.png"
+import "../assets/styles/About.scss"
+
 function About() {
     return (
         <>
         <Header/>
         <main>
             <div className="container">
-                <img className="container_image" src={image} alt="paysage"/>
+                <div className="background">
+                    <img className="background__image" src={image} alt="paysage"/>
+                </div>
                 <section className="all">
                     <div className="all__bar">
                         <div>
                             <p>Fiabilité</p>
-                            <span><i class="fa-solid fa-chevron-up"></i></span>
+                            <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
                         <div>
                             <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
@@ -21,7 +25,7 @@ function About() {
                     <div className="all__bar">
                         <div>
                             <p>Respect</p>
-                            <span><i class="fa-solid fa-chevron-up"></i></span>
+                            <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
                         <div>
                             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -30,7 +34,7 @@ function About() {
                     <div className="all__bar">
                         <div>
                             <p>Service</p>
-                            <span><i class="fa-solid fa-chevron-up"></i></span>
+                            <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
                         <div>
                             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -39,7 +43,7 @@ function About() {
                     <div className="all__bar">
                         <div>
                             <p>Sécurité</p>
-                            <span><i class="fa-solid fa-chevron-up"></i></span>
+                            <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
                         <div>
                             <p>La sécurité est la priorité de Kasa.</p>
--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom"
 import Header from './Header'
 import Footer from './Footer'
-import '../assets/styles/Error.scss'
+import styles from '../assets/styles/Error.module.scss'
 
 function Error() {
     return (
         <>
         <Header/>
-        <div className="container">
-            <p className="container__error">404</p>
-            <p className="container__message">Oups! La page que vous demandez n'existe pas.</p>
-            <p className="container__link"><Link to="/">Retourner sur la page d'accueil</Link></p>
+        <div className={styles.container}>
+            <p className={styles.container__error}>404</p>
+            <p className={styles.container__message}>Oups! La page que vous demandez n'existe pas.</p>
+            <p className={styles.container__link}><Link to="/">Retourner sur la page d'accueil</Link></p>
         </div>
         <Footer/>
         </>
