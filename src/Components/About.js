27d84c7a70bd4ea3f6b326385f58db2e2383d3af
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-13 22:07:05 +0200

    feat: about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,40 +12,40 @@ function About() {
                 <div className={styles.background}>
                     <img className={styles.background__image} src={image} alt="paysage"/>
                 </div>
-                <section className={styles.all}>
-                    <div className={styles.all__bar}>
-                        <div>
+                <section className={styles.collapse}>
+                    <div className={styles.collapse__bar}>
+                        <div className={styles.collapse__bar__title}>
                             <p>Fiabilité</p>
-                            <span><i className="styles.fa-solid fa-chevron-up"></i></span>
+                            <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
-                        <div>
+                        <div className={styles.collapse__bar__content}>
                             <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
                         </div>
                     </div>
-                    <div className={styles.all__bar}>
-                        <div>
+                    <div className={styles.collapse__bar}>
+                        <div className={styles.collapse__bar__title}>
                             <p>Respect</p>
                             <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
-                        <div>
+                        <div className={styles.collapse__bar__content}>
                             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                         </div>
                     </div>
-                    <div className={styles.all__bar}>
-                        <div>
+                    <div className={styles.collapse__bar}>
+                        <div className={styles.collapse__bar__title}>
                             <p>Service</p>
                             <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
-                        <div>
+                        <div className={styles.collapse__bar__content}>
                             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                         </div>
                     </div>
-                    <div className={styles.all__bar}>
-                        <div>
+                    <div className={styles.collapse__bar}>
+                        <div className={styles.collapse__bar__title}>
                             <p>Sécurité</p>
                             <span><i className="fa-solid fa-chevron-up"></i></span>
                         </div>
-                        <div>
+                        <div className={styles.collapse__bar__content}>
                             <p>La sécurité est la priorité de Kasa.</p>
                         </div>
                     </div>
