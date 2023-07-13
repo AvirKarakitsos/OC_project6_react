9b9e378f57059c08ae5db867a9552a1e0844a97d
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-13 15:37:57 +0200

    feat: home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,18 @@
+import { useEffect, useState } from 'react'
 import background from '../assets/images/image-source-1.png'
 import '../assets/styles/Home.scss'
 import Footer from './Footer'
 import Header from './Header'
+import jsonFile from '../assets/api/logements.json'
 
 
 function Home() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        let table = jsonFile.slice(0,6)
+        setData(table)
+    },[])
+
     return (
         <>
         <Header/>
@@ -13,12 +21,16 @@ function Home() {
                 <img className="background__image" src={background} alt="paysage"/>
                 <h1 className='background__title'>Chez vous, partout et ailleurs</h1>
             </div>
-            <section>
-                <div className='container'>
-                    <article>
-                        <img src="#" alt="logement"/>
-                        <h2>Titre logement</h2>
-                    </article>
+            <section className='gallery'>
+                <div className='gallery__container'>
+                    {data.map((inputValue => {
+                        return(
+                            <article className='gallery__container__articles' key={inputValue.id}>
+                                <img className='gallery__container__articles__image' src={inputValue.cover} alt="logement"/>
+                                <h2 className='gallery__container__articles__title'>{inputValue.title}</h2>
+                            </article>    
+                        )
+                    }))}
                 </div>
             </section>
         </main>
