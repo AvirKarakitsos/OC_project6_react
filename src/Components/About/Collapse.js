ba50832117911e8834cf170453385d35ab2baf7c
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-14 10:08:36 +0200

    feat: collapse animation

--- src/Components/About/Collapse.js
+++ src/Components/About/Collapse.js
@@ -23,16 +23,36 @@ function Collapse() {
             content: "La sécurité est la priorité de Kasa."
         }
     ]
+    function handleShow(id) {
+        let arrow = document.querySelector(`.arrow${id}`)
+        let content = document.querySelector(`.content${id}`)
+        
+        arrow.classList.toggle("rotate")   
+        
+        if (arrow.classList.contains("rotate")) {
+            arrow.style.transform = "rotate(-180deg)" 
+            arrow.style.transition = "transform 1000ms" 
+            content.style.transform = "translateY(0)" 
+            content.style.height = "80px"
+            content.style.transition = "all 1000ms"
+        } else {
+            arrow.style.transform = "rotate(0deg)" 
+            arrow.style.transition = "transform 1000ms"
+            content.style.transform = "translateY(-52px)"
+            content.style.height = "0" 
+            content.style.transition = "all 1000ms"
+        }
+    }
 
     return (
         table.map(inputValue => {
             return (
-                <div key={inputValue.id} className={styles.collapse__bar}>
+                <div key={inputValue.id} className={"bar"+inputValue.id+" "+styles.collapse__bar}>
                     <div className={styles.collapse__bar__title}>
                         <p>{inputValue.title}</p>
-                        <span><i className="fa-solid fa-chevron-up"></i></span>
+                        <span><i className={"arrow"+inputValue.id+" fa-solid fa-chevron-up"} onClick={() => handleShow(inputValue.id)}></i></span>
                     </div>
-                    <div className={styles.collapse__bar__content}>
+                    <div className={"content"+inputValue.id+" "+styles.collapse__bar__content}>
                         <p>{inputValue.content}</p>
                     </div>
                 </div>
