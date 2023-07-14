17d7023e53a5d2c1b9c37ec97ee3e161266cbb70
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-14 21:24:22 +0200

    feat: slides counter

--- src/Components/Home/Slideshow.js
+++ src/Components/Home/Slideshow.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import styles from '../../assets/styles/Slideshow.module.scss'
 
 function Slideshow({images}) {
     let numberImages = images.length
+    const [counter, setCounter] = useState(1)
+
+    function handleNext() {
+        counter < numberImages ? setCounter(counter + 1) : setCounter(1)
+    }
+    function handlePrevious() {
+        counter !== 1 ? setCounter(counter - 1) : setCounter(numberImages)
+    }
 
     return (
     <div className={styles.slideContainer}>
         {images.map((image,index) => index === 0 ? <img height={415} src={image} className={styles.images+" "+styles.active} alt="photos appartements" key={index}/> : <img height={415} src={image} className={styles.images} alt="photos appartements" key={index}/>)}
-        <button className={styles.btn+" "+styles.previous}><i className="fa-solid fa-chevron-left icon"></i></button>
-        <button className={styles.btn+" "+styles.next}><i className="fa-solid fa-chevron-right icon"></i></button>
-        <div className={styles.slide}>{"/"+numberImages}</div>
+        <button onClick={handlePrevious} className={styles.btn+" "+styles.previous}><i className="fa-solid fa-chevron-left icon"></i></button>
+        <button onClick={handleNext} className={styles.btn+" "+styles.next}><i className="fa-solid fa-chevron-right icon"></i></button>
+        <div className={styles.slide}>{counter+"/"+numberImages}</div>
     </div>
     )
 }
