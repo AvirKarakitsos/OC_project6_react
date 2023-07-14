a69706ef49743096491eb9b8f96fa1c5c7fa0944
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-14 22:05:05 +0200

    feat: carousel

--- src/Components/Home/Slideshow.js
+++ src/Components/Home/Slideshow.js
@@ -6,19 +6,61 @@ function Slideshow({images}) {
     const [counter, setCounter] = useState(1)
 
     function handleNext() {
-        counter < numberImages ? setCounter(counter + 1) : setCounter(1)
+        let actual
+        let next
+
+        if(counter < numberImages) {
+            actual = document.getElementById(`image${counter-1}`)
+            next= document.getElementById(`image${counter}`)
+            
+            actual.classList.remove("Slideshow_active__exnB9")
+            next.classList.add("Slideshow_active__exnB9")
+
+            setCounter(counter + 1)
+        } else {
+            actual = document.getElementById(`image${counter-1}`)
+            next= document.getElementById(`image${0}`)
+
+            actual.classList.remove("Slideshow_active__exnB9")
+            next.classList.add("Slideshow_active__exnB9")
+
+            setCounter(1)
+        }
+        
     }
     function handlePrevious() {
-        counter !== 1 ? setCounter(counter - 1) : setCounter(numberImages)
+        let actual
+        let next
+
+        if(counter !== 1) {
+            actual = document.getElementById(`image${counter-1}`)
+            next= document.getElementById(`image${counter-2}`)
+            
+            actual.classList.remove("Slideshow_active__exnB9")
+            next.classList.add("Slideshow_active__exnB9")
+
+            setCounter(counter - 1)
+        } else {
+            actual = document.getElementById(`image${counter-1}`)
+            next= document.getElementById(`image${numberImages-1}`)
+            
+            actual.classList.remove("Slideshow_active__exnB9")
+            next.classList.add("Slideshow_active__exnB9")
+
+            setCounter(numberImages)
+        }
     }
 
     return (
-    <div className={styles.slideContainer}>
-        {images.map((image,index) => index === 0 ? <img height={415} src={image} className={styles.images+" "+styles.active} alt="photos appartements" key={index}/> : <img height={415} src={image} className={styles.images} alt="photos appartements" key={index}/>)}
-        <button onClick={handlePrevious} className={styles.btn+" "+styles.previous}><i className="fa-solid fa-chevron-left icon"></i></button>
-        <button onClick={handleNext} className={styles.btn+" "+styles.next}><i className="fa-solid fa-chevron-right icon"></i></button>
-        <div className={styles.slide}>{counter+"/"+numberImages}</div>
-    </div>
+        <div className={styles.slideContainer}>
+            {images.map((image,index) => index === 0 ? <img height={415} src={image} id={"image"+index} className={styles.images+" "+styles.active} alt="photos appartements" key={index}/> : <img height={415} src={image} id={"image"+index} className={styles.images} alt="photos appartements" key={index}/>)}
+            {numberImages !== 1 ? 
+                <><button onClick={handlePrevious} className={styles.btn+" "+styles.previous}><i className="fa-solid fa-chevron-left icon"></i></button>
+                <button onClick={handleNext} className={styles.btn+" "+styles.next}><i className="fa-solid fa-chevron-right icon"></i></button>
+                <div className={styles.slide}>{counter+"/"+numberImages}</div></>
+                : null
+            }
+        </div>
     )
 }
 
