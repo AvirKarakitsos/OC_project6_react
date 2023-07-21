af9372d87a3d62d59b29386155b91412f8525329
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-21 17:12:11 +0200

    delete: App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Router from './Components/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
 
