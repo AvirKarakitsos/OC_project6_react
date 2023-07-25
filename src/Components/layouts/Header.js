17cc14c906165c38c9176d4480f2f5731229ed7e
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-07-25 15:48:30 +0200

    refactor: module for Header scss

--- src/Components/layouts/Header.js
+++ src/Components/layouts/Header.js
@@ -1,4 +1,4 @@
-import '../../assets/styles/Header.scss'
+import styles from'../../assets/styles/Header.module.scss'
 import logo from '../../assets/images/logo-light.png'
 import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
@@ -24,25 +24,25 @@ function Header() {
     },[windowWidth])
     
     return (
-        <header className='header'>
-            <img className='header__logo' src={logo} alt='logo'/>
-            <nav className='header__navbar'>
+        <header className={styles.header}>
+            <img className={styles.header__logo} src={logo} alt='logo'/>
+            <nav className={styles.header__navbar}>
                 {location.pathname === "/"
-                && <ul className='header__navbar__list'>
+                && <ul className={styles.header__navbar__list}>
                      <li>{home}</li>
                      <li><Link to='/about'>{about}</Link></li>
                 </ul>
                 }
                 {location.pathname === "/about"
-                && <ul className='header__navbar__list'>
+                && <ul className={styles.header__navbar__list}>
                      <li><Link to='/'>{home}</Link></li>
                      <li>{about}</li>
                 </ul>
                 }
                 {location.pathname !== "/" && location.pathname !== "/about"
-                && <ul className='header__navbar__list'>
-                     <li><Link className='link' to='/'>{home}</Link></li>
-                     <li><Link className='link' to='/about'>{about}</Link></li>
+                && <ul className={styles.header__navbar__list}>
+                     <li><Link className={styles.link} to='/'>{home}</Link></li>
+                     <li><Link className={styles.link} to='/about'>{about}</Link></li>
                 </ul>
                 }
 
